18d596a29e178c56012f45980b5445f392a45726
Thomas O'Neill <tom@opensystemslab.io>
2022-10-24 14:52:00 +0100

    loadAsync not load

--- src/App.tsx
+++ src/App.tsx
@@ -30,133 +30,138 @@ function App() {
   const models = useRef<IFCModel[]>([])
 
   useEffect(() => {
-    if (i === 0) {
-      i++
-      return
-    }
-    if (!rootRef.current) return
-    console.log("useEffect called")
+    const go = async () => {
+      if (i === 0) {
+        i++
+        return
+      }
+      if (!rootRef.current) return
 
-    const scene = new Scene()
-    const camera = new PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    )
+      console.log("useEffect called")
 
-    const light = new AmbientLight()
+      const scene = new Scene()
+      const camera = new PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      )
 
-    scene.add(light)
+      const light = new AmbientLight()
 
-    const controls = new OrbitControls(camera, renderer.domElement)
+      scene.add(light)
 
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    renderer.setClearColor("white")
+      const controls = new OrbitControls(camera, renderer.domElement)
 
-    rootRef.current.appendChild(renderer.domElement)
+      renderer.setSize(window.innerWidth, window.innerHeight)
+      renderer.setClearColor("white")
 
-    camera.position.z = 5
+      rootRef.current.appendChild(renderer.domElement)
 
-    const ifcLoader = new IFCLoader()
-    const ifc = ifcLoader.ifcManager
-    ifc.setWasmPath("../../../wasm/")
-    ifc.setupThreeMeshBVH(
-      computeBoundsTree,
-      disposeBoundsTree,
-      acceleratedRaycast
-    )
+      camera.position.z = 5
 
-    const group1 = new Group()
-    group1.position.x = -5
-    scene.add(group1)
+      const ifcLoader = new IFCLoader()
+      const ifc = ifcLoader.ifcManager
+      ifc.setWasmPath("../../../wasm/")
+      ifc.setupThreeMeshBVH(
+        computeBoundsTree,
+        disposeBoundsTree,
+        acceleratedRaycast
+      )
 
-    const group2 = new Group()
-    group2.position.x = 5
-    scene.add(group2)
+      const group1 = new Group()
+      group1.position.x = -5
+      scene.add(group1)
 
-    ifcLoader.load(ifcUrl, (ifcModel) => {
+      const group2 = new Group()
+      group2.position.x = 5
+      scene.add(group2)
+
+      const ifcModel = await ifcLoader.loadAsync(ifcUrl)
+
       models.current.push(ifcModel)
       group1.add(ifcModel)
       const clone = ifcModel.clone()
       models.current.push(clone)
       group2.add(clone)
-    })
 
-    const raycaster = new Raycaster()
-    raycaster.firstHitOnly = true
-    const mouse = new Vector2()
+      const raycaster = new Raycaster()
+      raycaster.firstHitOnly = true
+      const mouse = new Vector2()
 
-    function cast(event: any) {
-      // Computes the position of the mouse on the screen
-      const bounds = renderer.domElement.getBoundingClientRect()
+      function cast(event: any) {
+        // Computes the position of the mouse on the screen
+        const bounds = renderer.domElement.getBoundingClientRect()
 
-      const x1 = event.clientX - bounds.left
-      const x2 = bounds.right - bounds.left
-      mouse.x = (x1 / x2) * 2 - 1
+        const x1 = event.clientX - bounds.left
+        const x2 = bounds.right - bounds.left
+        mouse.x = (x1 / x2) * 2 - 1
 
-      const y1 = event.clientY - bounds.top
-      const y2 = bounds.bottom - bounds.top
-      mouse.y = -(y1 / y2) * 2 + 1
+        const y1 = event.clientY - bounds.top
+        const y2 = bounds.bottom - bounds.top
+        mouse.y = -(y1 / y2) * 2 + 1
 
-      // Places it on the camera pointing to the mouse
-      raycaster.setFromCamera(mouse, camera)
+        // Places it on the camera pointing to the mouse
+        raycaster.setFromCamera(mouse, camera)
 
-      // Casts a ray
-      return raycaster.intersectObjects(models.current)
-    }
-
-    const preselectMat = new MeshLambertMaterial({
-      transparent: true,
-      opacity: 0.6,
-      color: 0xff88ff,
-      depthTest: false,
-    })
-
-    // Reference to the previous selection
-    let preselectModel = { id: -1 }
-
-    function highlight(event: any, material: any, model: any) {
-      const found: any = cast(event)[0]
-      if (found) {
-        console.log(found.object.modelID)
-        console.log(ifc.state.models)
-
-        // Gets model ID
-        model.id = found.object.modelID
-
-        // if (!(found.object.modelID in ifc.state.models)) {
-        //   ifc.state.models[found.object.modelID] = found.object
-        // }
-
-        // Gets Express ID
-        const index = found.faceIndex
-        const geometry = found.object.geometry
-        const id = ifc.getExpressId(geometry, index)
-
-        // Creates subset
-        ifcLoader.ifcManager.createSubset({
-          modelID: model.id,
-          ids: [id],
-          material: material,
-          scene: scene,
-          removePrevious: true,
-        })
-      } else {
-        // Removes previous highlight
-        ifc.removeSubset(model.id, material)
+        // Casts a ray
+        return raycaster.intersectObjects(models.current)
       }
+
+      const preselectMat = new MeshLambertMaterial({
+        transparent: true,
+        opacity: 0.6,
+        color: 0xff88ff,
+        depthTest: false,
+      })
+
+      // Reference to the previous selection
+      let preselectModel = { id: -1 }
+
+      function highlight(event: any, material: any, model: any) {
+        const found: any = cast(event)[0]
+        if (found) {
+          console.log(found.object.modelID)
+          console.log(ifc.state.models)
+
+          // Gets model ID
+          model.id = found.object.modelID
+
+          // if (!(found.object.modelID in ifc.state.models)) {
+          //   ifc.state.models[found.object.modelID] = found.object
+          // }
+
+          // Gets Express ID
+          const index = found.faceIndex
+          const geometry = found.object.geometry
+          const id = ifc.getExpressId(geometry, index)
+
+          // Creates subset
+          ifcLoader.ifcManager.createSubset({
+            modelID: model.id,
+            ids: [id],
+            material: material,
+            scene: scene,
+            removePrevious: true,
+          })
+        } else {
+          // Removes previous highlight
+          ifc.removeSubset(model.id, material)
+        }
+      }
+
+      function animate() {
+        requestAnimationFrame(animate)
+        renderer.render(scene, camera)
+        controls.update()
+      }
+
+      renderer.domElement.onmousemove = (event) =>
+        highlight(event, preselectMat, preselectModel)
+      animate()
     }
 
-    function animate() {
-      requestAnimationFrame(animate)
-      renderer.render(scene, camera)
-      controls.update()
-    }
-
-    renderer.domElement.onmousemove = (event) =>
-      highlight(event, preselectMat, preselectModel)
-    animate()
+    go()
   }, [renderer])
 
   return (
